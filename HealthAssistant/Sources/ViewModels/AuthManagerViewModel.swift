1c931c0bbd194d3e841052e40b16102cb9761db4
Ladislas de Toldi <ladislas@detoldi.me>
2025-03-19 16:51:54 +0100

    ✨ (auth): Create AuthManagerViewModel

---
+++ HealthAssistant/Sources/ViewModels/AuthManagerViewModel.swift
@@ -0,0 +1,10 @@
+// Health Assistant App
+// Copyright ACME Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+@Observable
+class AuthManagerViewModel {
+    var isAuthenticated = false
+}
