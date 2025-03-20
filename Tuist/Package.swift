f8af28d486927744291af8a6d90c726c9d3e58e3
Ladislas de Toldi <ladislas@detoldi.me>
2025-03-21 00:26:04 +0100

    📌 (Packages): Add Firebase v11.10.0 dependency

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,5 +18,9 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            exact: "11.10.0"
+        ),
     ]
 )
