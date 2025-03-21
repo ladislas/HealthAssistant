6f361e1a2af39b77e62b26b470e82aa185eaf93e
Ladislas de Toldi <ladislas@detoldi.me>
2025-03-21 15:48:13 +0100

    ✨ (consent): Handle user consent in files view

--- HealthAssistant/Sources/Views/Tabs/FilesView.swift
+++ HealthAssistant/Sources/Views/Tabs/FilesView.swift
@@ -5,18 +5,62 @@
 import SwiftUI
 
 struct FilesView: View {
+    // MARK: Internal
+
     var body: some View {
         VStack {
-            Text("Files View")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(.orange)
+            if self.askForConsent {
+                VStack(spacing: 50) {
+                    Text("Your consent is required\nto upload and view files")
+                        .font(.callout)
+                        .bold()
+                        .multilineTextAlignment(.center)
+
+                    Button("Give Consent") {
+                        Task {
+                            try await self.consentManager.giveUserConsent(id: self.authManager.uid)
+                            if try await self.consentManager.hasUserGivenConsent(id: self.authManager.uid) {
+                                print("User has given consent")
+                                self.askForConsent = false
+                            }
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            } else {
+                Button {
+                    print("Upload file")
+                } label: {
+                    Label("Upload File", systemImage: "arrow.up.doc")
+                        .font(.title)
+                }
+            }
         }
         .navigationTitle("Files")
+        .onAppear {
+            Task {
+                let consentGiven = try await self.consentManager.hasUserGivenConsent(id: self.authManager.uid)
+
+                if consentGiven {
+                    print("User has given consent")
+                } else {
+                    print("User has not given consent")
+                }
+            }
+        }
     }
+
+    // MARK: Private
+
+    @State private var askForConsent = true
+    @State private var consentManager = LocalStorageUserConsentManager()
+
+    @Environment(AuthManager.self) var authManager: AuthManager
 }
 
 #Preview {
+    @Previewable var authManager = AuthManager.mock
+
     TabView {
         Tab("Files", systemImage: "doc") {
             NavigationStack {
@@ -24,4 +68,5 @@ struct FilesView: View {
             }
         }
     }
+    .environment(authManager)
 }
